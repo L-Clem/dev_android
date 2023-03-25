6046029778518f4a61ac6aeef91af3fb9f9c2f00
Clément <japanclems287@gmail.com>
2023-03-25 15:54:54 +0100

    Add kotlin

---
+++ app/src/main/java/com/example/devandroid/BaseActivity.kt
@@ -0,0 +1,22 @@
+package com.example.devandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+
+open class BaseActivity : AppCompatActivity() {
+    fun setHeaderTitle( title:String?){
+        val textViewTitle= findViewById<TextView>(R.id.textViewTitle)
+        textViewTitle.setText(title)
+    }
+
+    fun showBack(){
+        val imageViewBack=findViewById<ImageView>(R.id.imageViewBack)
+        imageViewBack.visibility= View.VISIBLE
+        imageViewBack.setOnClickListener(View.OnClickListener {
+            finish()
+        })
+    }
+}
--- app/src/main/java/com/example/devandroid/MainActivity.kt
+++ app/src/main/java/com/example/devandroid/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.devandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.view.View
+import android.content.Intent
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setHeaderTitle(getString(R.string.txt_epsi))
+
+        val buttonInfo = findViewById<Button>(R.id.buttonInfo)
+        buttonInfo.setOnClickListener(View.OnClickListener {
+            val newIntent = Intent(application, GroupActivity::class.java)
+            newIntent.putExtra("title", getString(R.string.txt_info))
+
+            startActivity(newIntent)
+        })
+
+        val buttonProduits = findViewById<Button>(R.id.buttonProduits)
+        buttonProduits.setOnClickListener(View.OnClickListener {
+            val newIntent = Intent(application, CategoriesActivity::class.java)
+            newIntent.putExtra("title", getString(R.string.txt_produits))
+            startActivity(newIntent)
+        })
     }
 }
