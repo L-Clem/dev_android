2b6c34df94968d2fac2fe7d2195fa9cb21638131
HugoFerry <hugoferry0103@gmail.com>
2023-04-02 17:06:26 +0200

    Miscellaneous and Corrections

--- app/src/main/java/com/example/devandroid/CategoriesActivity.kt
+++ app/src/main/java/com/example/devandroid/CategoriesActivity.kt
@@ -3,9 +3,10 @@ package com.example.devandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class CategoriesActivity : AppCompatActivity() {
+class CategoriesActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_categories)
+
     }
 }
