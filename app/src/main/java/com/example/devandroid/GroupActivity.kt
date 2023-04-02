bf3236e58c15f93d6598101a943cc887e8e6d8d0
HugoFerry <hugoferry0103@gmail.com>
2023-04-02 17:05:27 +0200

    Add GroupActivity

--- app/src/main/java/com/example/devandroid/GroupActivity.kt
+++ app/src/main/java/com/example/devandroid/GroupActivity.kt
@@ -1,11 +1,44 @@
 package com.example.devandroid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import org.json.JSONObject
+import java.io.IOException
+import java.io.InputStream
 
-class GroupActivity : AppCompatActivity() {
+class GroupActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_group)
+
+        setHeaderTitle(getString(R.string.txt_info))
+        showBack()
+
+
+        val buttonEtudiant1 = findViewById<Button>(R.id.etudiant1)
+        buttonEtudiant1.setOnClickListener(View.OnClickListener {
+            val newIntent = Intent(application, StudentActivity::class.java)
+            newIntent.putExtra("student", "etudiant1")
+            startActivity(newIntent)
+        })
+
+        val buttonEtudiant2 = findViewById<Button>(R.id.etudiant2)
+        buttonEtudiant2.setOnClickListener(View.OnClickListener {
+            val newIntent = Intent(application, StudentActivity::class.java)
+            newIntent.putExtra("student", "etudiant2")
+            startActivity(newIntent)
+        })
+
+        val buttonEtudiant3 = findViewById<Button>(R.id.etudiant3)
+        buttonEtudiant3.setOnClickListener(View.OnClickListener {
+            val newIntent = Intent(application, StudentActivity::class.java)
+            newIntent.putExtra("student", "etudiant3")
+            startActivity(newIntent)
+        })
+
     }
+
 }
