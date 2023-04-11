905c17ab3a5e6d8873e38ec5cb2af5edae893986
Clément <japanclems287@gmail.com>
2023-04-11 21:20:42 +0200

    product

--- app/src/main/java/com/example/devandroid/ProductDetailsActivity.kt
+++ app/src/main/java/com/example/devandroid/ProductDetailsActivity.kt
@@ -2,10 +2,21 @@ package com.example.devandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
-class ProductDetailsActivity : AppCompatActivity() {
+class ProductDetailsActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_details)
+        showBack()
+        setHeaderTitle(intent.extras?.getString("name"))
+
+        val editAvatar = findViewById<ImageView>(R.id.imageViewProduct)
+        editAvatar.setImageResource(R.drawable.logo_marche)
+
+        val editDescription = findViewById<TextView>(R.id.textViewProductDescription)
+        editDescription.setText(intent.extras?.getString("description"))
     }
 }
