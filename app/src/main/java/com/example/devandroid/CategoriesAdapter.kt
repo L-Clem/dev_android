e2100dbfef006d3e0cedce71a34d03dad808f5ca
Clément <japanclems287@gmail.com>
2023-04-13 22:25:56 +0200

    categories

--- app/src/main/java/com/example/devandroid/CategoriesAdapter.kt
+++ app/src/main/java/com/example/devandroid/CategoriesAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.devandroid
 
+import android.content.Context
 import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,19 +10,16 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.NonDisposableHandle.parent
 
 
 class CategoriesAdapter(val categories: ArrayList<Category>) :
     RecyclerView.Adapter<CategoriesAdapter.ViewHolder>() {
-    
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val layoutContent: LinearLayout = view.findViewById<LinearLayout>(R.id.layoutContentCategories)
-        val textViewTitle: TextView = view.findViewById<TextView>(R.id.textViewTitle)
-    }
+
     override fun onCreateViewHolder(
         viewGroup: ViewGroup,
         viewType: Int
-    ): CategoriesAdapter.ViewHolder {
+    ): ViewHolder {
         val view = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.cell_category, viewGroup, false)
         return ViewHolder(view)
@@ -29,23 +27,27 @@ class CategoriesAdapter(val categories: ArrayList<Category>) :
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val category = categories[position]
-        holder.textViewTitle.id = category.category_id
+        Log.e("DevAI","#######################  "+ category.title)
         holder.textViewTitle.text = category.title
-
-        holder.layoutContent.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View) {
-                Log.e("Epsi", "################# clickkk")
-                val newIntent: Intent =  Intent(holder.layoutContent.context, ProductsActivity::class.java)
-                newIntent.putExtra("category", category.title)
-                holder.layoutContent.context.startActivity(newIntent)
-            }
-        })
-
-        Log.e("Epsi", "################# click " + holder.layoutContent)
+        holder.itemView.setOnClickListener {
+            val intent = createIntentForItem(category, holder.itemView.context)
+            holder.itemView.context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
         return categories.size
+    }
+
+    private fun createIntentForItem(item: Category, context: Context): Intent {
+        val intent = Intent(context, ProductsActivity::class.java)
+        intent.putExtra("category", item.title)
+        return intent
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val layoutContent: LinearLayout = itemView.findViewById<LinearLayout>(R.id.layoutContentCategories)
+        val textViewTitle: TextView = itemView.findViewById<TextView>(R.id.textViewTitle)
     }
 }
 
